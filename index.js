eb59fd70cae049765aedeeefc10a96876e29c48d
Guy Ambar <guy@meetleo.co>
2023-05-25 16:14:37 +0300

    update index.js and added env

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "Say this is a test",
-  temperature: 0,
-  max_tokens: 7,
-});
+
+const asyncFunc = async () => {
+  let response;
+
+  try {
+    response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      temperature: 0,
+      max_tokens: 7,
+    });
+    
+  } catch (e) {
+    if (e.response?.data) {
+      console.error(e.response?.data)
+    }
+    throw e;
+  }
+  
+  console.log(response);
+}
+
+asyncFunc().catch(e => console.error(e));
